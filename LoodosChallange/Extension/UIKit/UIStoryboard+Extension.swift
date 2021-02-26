bc8122916d3bc9983f6209a4e10566b3fa25ca08
Yunus MAKAN <makanyunus@gmail.com>
2021-02-27 00:34:03 +0300

    Initial commit

---
+++ LoodosChallange/Extension/UIKit/UIStoryboard+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIStoryboard+Extension.swift
+//  LoodosChallange
+//
+//  Created by MAKAN on 27.02.2021.
+//
+
+import UIKit
+
+public extension UIStoryboard {
+    
+    static var myStoryboardName: UIStoryboard {
+        
+        return UIStoryboard(name: "Main", bundle: .main)
+    }
+
